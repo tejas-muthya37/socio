d2b64b1c4fe128d11a4e3b97826bb842e607534c
tejas-muthya37 <55662912+tejas-muthya37@users.noreply.github.com>
2022-05-08 14:20:57 +0530

    Feat: Feed layout

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,10 +1,16 @@
 import "./feed.css";
 import CreatePost from "./../CreatePost/CreatePost";
+import Post from "../Post/Post";
 
 const Feed = () => {
   return (
     <div className="Feed">
       <CreatePost />
+      <h2>Latest Posts</h2>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
     </div>
   );
 };
---
+++ src/Components/Post/Post.js
@@ -0,0 +1,35 @@
+import "./post.css";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+import { BsHeart } from "react-icons/bs";
+import { VscComment } from "react-icons/vsc";
+import { BsBookmark } from "react-icons/bs";
+
+const Post = () => {
+  return (
+    <div className="Post">
+      <div className="post-section">
+        <div className="author-display-picture"></div>
+        <div className="post-body">
+          <p className="author-credentials">
+            <span>Tanay Pratap </span>@tanaypratap
+          </p>
+          <p className="post-content">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <div className="post-footer">
+            <BsHeart fontSize="larger" />
+            <VscComment fontSize="larger" />
+            <BsBookmark fontSize="larger" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
